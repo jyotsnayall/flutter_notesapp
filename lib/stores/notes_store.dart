9aea175edad0f2d98445be9a838d246f250d7002
jyotsnayall <jyotsnasharma2603@gmail.com>
2024-02-08 12:25:22 +0530

    updated crud operations to only hive box

--- lib/stores/notes_store.dart
+++ lib/stores/notes_store.dart
@@ -36,19 +36,14 @@ abstract class _NotesStore with Store {
 
   @action
   void getNotes() {
-    final notes = box.values.toList().cast<NotesModel>();
-    // for (var note in notes) {
-    //   print(note.title);
-    // }
+    notes = box.values.toList().cast<NotesModel>().asObservable();
   }
 
   @action
   void addNote(final String title, final String description) {
     final note = NotesModel(title: title, description: description);
-    notes.add(note);
-
-    // save to hive box
     box.add(note);
+    getNotes();
   }
 
   @action
@@ -56,27 +51,24 @@ abstract class _NotesStore with Store {
       final String description) async {
     final updatedNote = NotesModel(title: title, description: description);
 
-    final index = notes.indexWhere((element) => element.title == note.title);
-    print(index);
-    if (index != -1) {
-      notes[index] = updatedNote;
-    }
+    // final index = notes.indexWhere((element) => element.title == note.title);
+    // print(index);
+    // if (index != -1) {
+    //   notes[index] = updatedNote;
+    // }
 
     await box.put(note.title, updatedNote);
+    getNotes();
   }
 
   @action
   Future<void> removeNote(NotesModel note) async {
-    print("gonna delete note");
-    notes.remove(note);
-    print("deleted note");
-
-    // delete from hive box
     final noteToRemove =
         box.values.firstWhere((element) => element.title == note.title);
 
     if (noteToRemove != null) {
       await noteToRemove.delete();
     }
+    getNotes();
   }
 }
